c99a344878c0a03bdaa25a5c16a16951d27446da
Victor Santana <emanuel.santana@alumnos.udg.mx>
2024-03-19 23:20:42 -0600

    first python  object

---
+++ proof.py
@@ -0,0 +1,11 @@
+class Operations:
+  def __init__(self,number_1,number_2):
+     self.number_1 = number_1
+     self.number_2 = number_2
+  
+  def sum(a,b)->int:
+     return a + b
+  
+  def rest(a,b)->int:
+     return a - b
+   
